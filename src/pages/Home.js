5c7fa79fa957e2b5c5458f82c4a052bfbabfc711
sissy5656 <supersissy5656@gmail.com>
2020-09-14 19:59:28 -0500

    working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Home.css";
+import black from "../assets/black.mp4";
+import color from "../assets/color.mp4";
+import beige from "../assets/beige.mp4";
+import background from "../assets/background.mp4";
 
 const Home = () => {
-  return <div>This is Home page</div>;
+  return (
+    <div className="home-page">
+      <div className="background-home">
+        <video autoPlay loop>
+          <source src={background} type="video/mp4" />
+        </video>
+      </div>
+      <div className="white-tone">
+        <video autoPlay loop>
+          <source src={black} type="video/mp4" />
+        </video>
+      </div>
+      <div className="beige-tone">
+        <video autoPlay loop>
+          <source src={beige} type="video/mp4" />
+        </video>
+      </div>
+      <div className="black-tone">
+        <video autoPlay loop>
+          <source src={color} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,19 +1,41 @@
-import React from "react";
-
-import ProductsRow from "../components/ProductsRow";
-import Filter from "../components/Filter";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import ProductsRow from "../components/ProductsRow";
+// import Filter from "../components/Filter";
+import { Link } from "react-router-dom";
 import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/app/products")
+      .then((response) => {
+        console.log(response);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        return error;
+      });
+  }, []);
+  console.log(products);
   return (
     <div className="shop-page">
-      <div>
-        <h1>this is my shop page. list all the products here</h1>
-        <Filter count="9" />
-        <ProductsRow />
-        <ProductsRow />
-        <ProductsRow />
-      </div>
+      <ul className="products">
+        {products.map((product) => (
+          <li key={product.id}>
+            <div className="product">
+              <Link to={"/product/" + product.id}>
+                <img className="product-image" src={product.image} alt="" />
+              </Link>
+              <div className="product-name">
+                <Link to={"/product/" + product.id}>{product.name}</Link>
+              </div>
+              <strong>{product.price}</strong>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
