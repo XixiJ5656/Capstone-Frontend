32ce8211267252311196ace0cb2b9fc9307e5826
sissy5656 <supersissy5656@gmail.com>
2020-09-24 09:13:37 -0500

    Updates

---
+++ precache-manifest.3bec9e4604c9ece9a95151c7e0cc68fc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4add57d517a36c9f42d050ae6c07809e",
+    "url": "/xyz-ecommerce-react/index.html"
+  },
+  {
+    "revision": "17adf13b183d4ae015f9",
+    "url": "/xyz-ecommerce-react/static/css/2.5dddc201.chunk.css"
+  },
+  {
+    "revision": "16f40689eca3f65a7d0a",
+    "url": "/xyz-ecommerce-react/static/css/main.abb7f051.chunk.css"
+  },
+  {
+    "revision": "17adf13b183d4ae015f9",
+    "url": "/xyz-ecommerce-react/static/js/2.a8f68f43.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/xyz-ecommerce-react/static/js/2.a8f68f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f40689eca3f65a7d0a",
+    "url": "/xyz-ecommerce-react/static/js/main.3d5a648d.chunk.js"
+  },
+  {
+    "revision": "7617bd55990c62d05fa0",
+    "url": "/xyz-ecommerce-react/static/js/runtime-main.d2298612.js"
+  },
+  {
+    "revision": "287c511bdc7c2d4944a9941e7043a7cc",
+    "url": "/xyz-ecommerce-react/static/media/adminhub.287c511b.svg"
+  },
+  {
+    "revision": "f0ef68a26b17de44183a0aa8e764a9b4",
+    "url": "/xyz-ecommerce-react/static/media/back.f0ef68a2.svg"
+  },
+  {
+    "revision": "6be8d51485b7d3969dd46be8dd44e29e",
+    "url": "/xyz-ecommerce-react/static/media/cart.6be8d514.svg"
+  },
+  {
+    "revision": "189a4077447e5198a40a6169d5d7f91c",
+    "url": "/xyz-ecommerce-react/static/media/cool.189a4077.mp4"
+  },
+  {
+    "revision": "c5ce571731003b92d7f76a59d7fd1ee8",
+    "url": "/xyz-ecommerce-react/static/media/credit-card.c5ce5717.svg"
+  },
+  {
+    "revision": "7c4874becf27655aef21cb85b6edb841",
+    "url": "/xyz-ecommerce-react/static/media/del.7c4874be.svg"
+  },
+  {
+    "revision": "77288926f5f20cb6093c65aba655b7a5",
+    "url": "/xyz-ecommerce-react/static/media/delivery.77288926.svg"
+  },
+  {
+    "revision": "f9cf7df2c1e8a715a992b10ab6aa662f",
+    "url": "/xyz-ecommerce-react/static/media/diam.f9cf7df2.svg"
+  },
+  {
+    "revision": "7ab388658082175ca54e2b44ef77b042",
+    "url": "/xyz-ecommerce-react/static/media/edit.7ab38865.svg"
+  },
+  {
+    "revision": "060bda02b657fe3365ed509285599e45",
+    "url": "/xyz-ecommerce-react/static/media/fun.060bda02.mp4"
+  },
+  {
+    "revision": "2b307e6303766dcfd5fd12c1ca219217",
+    "url": "/xyz-ecommerce-react/static/media/jeans.2b307e63.mp4"
+  },
+  {
+    "revision": "66c17b7b1857cfa5ce5d6749ef08bdd1",
+    "url": "/xyz-ecommerce-react/static/media/model.66c17b7b.mp4"
+  },
+  {
+    "revision": "0d222f65ce88cb1b19b0401eb76340a6",
+    "url": "/xyz-ecommerce-react/static/media/next.0d222f65.svg"
+  },
+  {
+    "revision": "65e7d97365b9767c5e605a0f767a4531",
+    "url": "/xyz-ecommerce-react/static/media/paypal.65e7d973.svg"
+  },
+  {
+    "revision": "291ab1afda7fa8eb7036a23d243e9e38",
+    "url": "/xyz-ecommerce-react/static/media/remove.291ab1af.svg"
+  },
+  {
+    "revision": "1dd70b4b1c2e11b95bcea2b080fcb47f",
+    "url": "/xyz-ecommerce-react/static/media/search.1dd70b4b.svg"
+  },
+  {
+    "revision": "1d4514e4cca94557dc59312f02176df2",
+    "url": "/xyz-ecommerce-react/static/media/signout.1d4514e4.svg"
+  },
+  {
+    "revision": "c4d096abe6ef07b61b9f41273d6da36e",
+    "url": "/xyz-ecommerce-react/static/media/user.c4d096ab.svg"
+  },
+  {
+    "revision": "9ece3b7222fc907afb44a1d9391d2b48",
+    "url": "/xyz-ecommerce-react/static/media/work.9ece3b72.mp4"
+  }
+]);
